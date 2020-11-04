fb18495ad51534d7d2ba44113a4b9b6d8374d62c
Timothy Claeys <timothy.claeys@gmail.com>
2020-11-04 15:58:01 +0100

    Update EDHOC roles

--- edhoc/roles/edhoc.py
+++ edhoc/roles/edhoc.py
@@ -1,109 +1,28 @@
-from abc import ABCMeta, abstractmethod
-from typing import Optional, Union, Callable, List
+from abc import ABCMeta
+from typing import List
 
-from cryptography.hazmat.backends import openssl
-from cryptography.hazmat.primitives.asymmetric import x25519
-from cryptography.hazmat.primitives.hashes import Hash
-from cryptography.hazmat.primitives.kdf.hkdf import HKDF
-from cryptography.hazmat.primitives.serialization import Encoding, PrivateFormat, NoEncryption, PublicFormat
-from cose.keys.cosekey import CoseEllipticCurves
-from cose.keys.ec import EC2
-from cose.keys.okp import OKP
+from cose import CoseEllipticCurves, CoseAlgorithms, KeyOps, OKP
+from cose.keys.cosekey import CoseKey
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PrivateKey
+from cryptography.hazmat.primitives.asymmetric.x448 import X448PrivateKey
 
-from edhoc.definitions import CipherSuite
+from edhoc.definitions import CipherSuite, Correlation
 
 
 class EdhocRole(metaclass=ABCMeta):
 
     def __init__(self,
-                 method_corr: Optional[int] = None,
-                 conn_idi: bytes = b'',
-                 conn_idr: bytes = b'',
-                 cred_id_r_type: Optional[int] = None,
-                 cred_r: bytes = b'',
-                 cred_id_i_type: Optional[int] = None,
-                 cred_i: bytes = b'',
-                 cipher_suite: Optional[CipherSuite] = None,
-                 aad1: bytes = b'',
-                 aad2: bytes = b'',
-                 aad3: bytes = b''):
-        self.method_corr = method_corr
-        self.conn_idi = conn_idi
-        self.conn_idr = conn_idr
-        self.cipher_suite = cipher_suite
+                 corr: Correlation,
+                 conn_id: bytes,
+                 cred_id_type,
+                 cred: bytes,
+                 supported_ciphers: List[CipherSuite]):
+        self.corr = corr
+        self.conn_id = conn_id
+        self.supported_ciphers = supported_ciphers
 
-        self.aad1 = aad1
-        self.aad2 = aad2
-        self.aad3 = aad3
+        self.cred = cred
+        self.cred_id_type = cred_id_type
 
-        self.cred_i = cred_i
-        self.cred_id_i_type = cred_id_i_type
-        self.cred_r = cred_r
-        self.cred_id_r_type = cred_id_r_type
-
-        self.callback_msg1: Optional[Callable] = None
-        self.callback_msg2: Optional[Callable] = None
-        self.callback_msg3: Optional[Callable] = None
-
-        self.priv_key: Optional[Union[EC2, OKP]] = None
-        self.pub_key: Optional[Union[EC2, OKP]] = None
-
-    @property
-    def callback_msg1(self) -> Callable[[], bytes]:
-        return self._callback_msg1
-
-    @callback_msg1.setter
-    def callback_msg1(self, func: Callable[[], bytes]):
-        self._callback_msg1 = func
-
-    @property
-    def callback_msg2(self) -> Callable[[], bytes]:
-        return self._callback_msg2
-
-    @callback_msg2.setter
-    def callback_msg2(self, func: Callable[[], bytes]) -> None:
-        self._callback_msg2 = func
-
-    @property
-    def callback_msg3(self) -> Callable[[], bytes]:
-        return self._callback_msg3
-
-    @callback_msg3.setter
-    def callback_msg3(self, func: Callable[[], bytes]) -> None:
-        self._callback_msg3 = func
-
-    def _compute_transcript(self, msg_parts: List[bytes]):
-        """ Computes the transcript hash """
-
-        hash_func = CipherSuite(self.cipher_suite).name.value.edhoc_hash
-
-        h = Hash(algorithm=hash_func(), backend=openssl.backend)
-
-        for part in msg_parts:
-            h.update(part)
-
-        return h.finalize()
-
-    def _compute_ecdh(self) -> bytes:
-        curve = CipherSuite(self.cipher_suite).name.value.edhoc_ecdh_curve
-
-        if isinstance(self.priv_key, OKP) and isinstance(self.pub_key, OKP) and curve == CoseEllipticCurves.X25519:
-            private_key = x25519.X25519PrivateKey.from_private_bytes(data=self.priv_key.d)
-            public_key = x25519.X25519PublicKey.from_public_bytes(data=self.pub_key.x)
-
-            return private_key.exchange(public_key)
-        elif isinstance(self.priv_key, OKP) and isinstance(self.pub_key, OKP) and curve == CoseEllipticCurves.P_256:
-            pass
-        else:
-            TypeError("Invalid key or curve type")
-
-    def _key_derivation(self, key: bytes, length=16, salt=b'', info=b''):
-        hash_func = CipherSuite(self.cipher_suite).value.edhoc_hash
-
-        hkdf = HKDF(algorithm=hash_func(),
-                    length=length,
-                    salt=salt,
-                    info=info,
-                    backend=openssl.backend)
-        return hkdf.derive(key)
 
--- edhoc/roles/responder.py
+++ edhoc/roles/responder.py
@@ -1,5 +1,65 @@
+from typing import TYPE_CHECKING, List, Callable, Optional
+
+from edhoc.definitions import Correlation, CipherSuite
+from edhoc.messages.error import MessageError
+from edhoc.messages.message1 import MessageOne
 from edhoc.roles.edhoc import EdhocRole
+
+if TYPE_CHECKING:
+    pass
 
 
 class Responder(EdhocRole):
-    pass
+    def __init__(self,
+                 corr: Correlation,
+                 conn_id: bytes,
+                 cred_id_type,
+                 cred: bytes,
+                 supported_ciphers: List[CipherSuite],
+                 aad1_cb: Optional[Callable] = None):
+        super(Responder, self).__init__(corr, conn_id, cred_id_type, cred, supported_ciphers)
+
+        self.aad1_cb = aad1_cb
+
+        # epher
+        self._ephemeral_key = None
+
+    def create_message_two(self, message_one: bytes) -> bytes:
+        """
+        Decodes an incoming EDHOC message 1 and creates and EDHOC message 2 or error message based on the content
+        of message 1.
+
+        :param message_one: Bytes representing an EDHOC message 1.
+        :returns: Bytes of an EDHOC message 2 or an EDHOC error message.
+        """
+
+        msg_1 = MessageOne.decode(message_one)
+
+        if not self._verify_cipher_selection(msg_1.selected_cipher, msg_1.cipher_suites):
+            return MessageError().encode()
+
+        if msg_1.aad1 != b'' and self.aad1_cb is not None:
+            self.aad1_cb(msg_1.aad1)
+
+    def _verify_cipher_selection(self, selected: CipherSuite, supported: List[CipherSuite]) -> bool:
+        """
+        Checks if the selected cipher suite is supported and that no prior cipher suites in the Initiator's list of
+        supported ciphers is supported by the Responder.
+
+        :param selected: the cipher suite selected by the Initiator
+        :param supported: the list of cipher suites supported by the Initiator
+        :return:
+        """
+        if selected not in self.supported_ciphers:
+            return False
+
+        for sc in supported:
+            if sc in self.supported_ciphers and sc != selected:
+                return False
+            elif sc in self.supported_ciphers and sc == selected:
+                return True
+            else:
+                continue
+
+        return True
+
