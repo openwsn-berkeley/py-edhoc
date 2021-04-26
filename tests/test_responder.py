7e0df8acc702c0aec3623e12e6b336d935fd85ed
chrysn <chrysn@fsfe.org>
2021-04-26 12:53:56 +0200

    Tests: Stop with a warning when vector key material is insufficient to finalize

--- tests/test_responder.py
+++ tests/test_responder.py
@@ -1,3 +1,5 @@
+import warnings
+
 import cbor2
 from cose.headers import KID
 from cose.keys import OKPKey
@@ -6,6 +8,9 @@ from edhoc.definitions import CipherSuite
 from edhoc.messages import MessageThree, MessageTwo, EdhocMessage
 from edhoc.messages.message1 import MessageOne
 
+class NoRemoteKey(UserWarning):
+    def __str__(self):
+        return "Skipping verification for lack of auth key"
 
 def test_responder_message2(responder, test_vectors):
     responder.msg_1 = MessageOne.decode(test_vectors['S']['message_1'])
@@ -55,6 +60,9 @@ def test_responder_finalize(responder, test_vectors):
         assert decoded[0] == cbor2.loads(test_vectors['I']['cred_id'])
     assert decoded[1] == test_vectors['S']['signature_3']
 
+    if getattr(responder, 'remote_authkey', None) is None:
+        warnings.warn(NoRemoteKey())
+        return
     c_i, c_r, app_aead, app_hash = responder.finalize(test_vectors['S']['message_3'])
 
     assert c_i == test_vectors['I']['conn_id']
