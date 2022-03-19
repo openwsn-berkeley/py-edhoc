cdaa14ef9628c27bd6b494233bb88f3024abba6c
chrysn <chrysn@fsfe.org>
2022-03-19 17:58:48 +0100

    message 2: Remove ID_I (this is now sent out-of-band all the way)

--- edhoc/messages/message2.py
+++ edhoc/messages/message2.py
@@ -13,7 +13,6 @@ class MessageTwo(EdhocMessage):
     CIPHERTEXT = -1
     CONN_ID_R = -2
     G_Y = -3
-    CONN_ID_I = -4
 
     @classmethod
     def decode(cls, received) -> 'MessageTwo':
@@ -29,25 +28,17 @@ class MessageTwo(EdhocMessage):
         conn_idr = cls.decode_bstr_id(decoded[cls.CONN_ID_R])
         g_y = decoded[cls.G_Y]
 
-        try:
-            conn_idi = cls.decode_bstr_id(decoded[cls.CONN_ID_I])
-        except IndexError:
-            conn_idi = b''
-
-        return cls(g_y, conn_idr, ciphertext, conn_idi)
+        return cls(g_y, conn_idr, ciphertext)
 
     @classmethod
-    def data_2(cls, g_y: bytes, conn_idr: bytes, corr: Correlation, conn_idi: Optional[bytes] = b'') -> 'CBOR':
+    def data_2(cls, g_y: bytes, conn_idr: bytes, corr: Correlation) -> 'CBOR':
         """ Create the data_2 message part. """
 
         data_2 = [g_y, EdhocMessage.encode_bstr_id(conn_idr)]
 
-        if corr == Correlation.CORR_0 or corr == Correlation.CORR_2:
-            data_2.insert(0, EdhocMessage.encode_bstr_id(conn_idi))
-
         return b''.join(cbor2.dumps(part) for part in data_2)
 
-    def __init__(self, g_y: bytes, conn_idr: bytes, ciphertext: bytes, conn_idi: bytes = b''):
+    def __init__(self, g_y: bytes, conn_idr: bytes, ciphertext: bytes):
         """
         Creates an EDHOC MessageTwo object.
         """
@@ -55,18 +46,13 @@ class MessageTwo(EdhocMessage):
         self.conn_idr = conn_idr
         self.g_y = g_y
         self.ciphertext = ciphertext
-        self.conn_idi = conn_idi
 
     def encode(self, corr: Correlation) -> bytes:
         """ Encode EDHOC message 2. """
 
-        return b''.join([self.data_2(self.g_y, self.conn_idr, corr, self.conn_idi), cbor2.dumps(self.ciphertext)])
+        return b''.join([self.data_2(self.g_y, self.conn_idr, corr), cbor2.dumps(self.ciphertext)])
 
     def __repr__(self) -> str:
-        if self.conn_idi != b'':
-            output = f'<MessageTwo: [{self.conn_idi}, {EdhocMessage._truncate(self.g_y)}, {self.conn_idr}, ' \
-                     f'{self.ciphertext}>'
-        else:
-            output = f'<MessageTwo: [{EdhocMessage._truncate(self.g_y)}, {self.conn_idr}, {self.ciphertext}>'
+        output = f'<MessageTwo: [{EdhocMessage._truncate(self.g_y)}, {self.conn_idr}, {self.ciphertext}>'
 
         return output
--- edhoc/roles/responder.py
+++ edhoc/roles/responder.py
@@ -197,7 +197,7 @@ class Responder(EdhocRole):
 
         self._generate_ephemeral_key()
 
-        self.msg_2 = MessageTwo(self.g_y, self.conn_idr, self.ciphertext_2, self.conn_idi)
+        self.msg_2 = MessageTwo(self.g_y, self.conn_idr, self.ciphertext_2)
 
         self._internal_state = EdhocState.MSG_2_SENT
         return self.msg_2.encode(self.corr)
